5033bfd373f77a49d6531f60b0bfde14f15a1876
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-10-15 11:17:10 +0300

    add loginController

--- controller/authController.js
+++ controller/authController.js
@@ -1,16 +1,26 @@
+const { login } = require('../services/authService');
+
 const authController = require ("express").Router();
 
 
 authController.get('/login', (req,res)=> {
-    const payload = {
-        _id:'635ghgvh552jhgfd',
-        username: 'peter', 
-        roles: ['user']
-    };
-    const token = jwt.sign(payload, jwtSecret, {expiresIn: '4h'}); //next time expirins 4h
-    res.cookie('jwt', token);
+   // const payload = {
+   //     _id:'635ghgvh552jhgfd',
+   //     username: 'peter', 
+   //     roles: ['user']
+   // };
+   // const token = jwt.sign(payload, jwtSecret, {expiresIn: '4h'}); //next time expirins 4h
+   // res.cookie('jwt', token);
     res.send("Here is your token")
 });
+
+authController.post('/login', async (req, res) =>{
+    const result = await login(req.body.username, req.body.password);
+    const token = req.signJwt(result);
+    res.cookie('jwt', token);
+    res.redirect('/');
+})
+
 
 
 
---
+++ services/authService.js
@@ -0,0 +1,17 @@
+async function login(username, password) {
+    new Promise((res, rej) => {
+        if(username.toLowerCase() == 'peter'&& password == '123'){
+            res({
+                _id: '29vhjvhjv1515',
+                username: 'Peter',
+                roles: ['user']
+            });
+        } else {
+            rej(new Error('Incorrect username or password'));
+        }
+    });
+}
+
+module.exports = {
+    login
+};
