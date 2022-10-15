3de9b924cb54af996224563a108048e98543a2c9
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-10-15 20:23:15 +0300

    add login and register

--- controller/authController.js
+++ controller/authController.js
@@ -1,36 +1,64 @@
-const { login } = require('../services/authService');
+const { login, register } = require("../services/authService");
 
-const authController = require ("express").Router();
+const authController = require("express").Router();
 
-
-authController.get('/login', (req,res)=> {
-   // const payload = {
-   //     _id:'635ghgvh552jhgfd',
-   //     username: 'peter', 
-   //     roles: ['user']
-   // };
-   // const token = jwt.sign(payload, jwtSecret, {expiresIn: '4h'}); //next time expirins 4h
-   // res.cookie('jwt', token);
-    res.render('loginPage', {
-        title:'Login Page'
-    })
+authController.get("/login", (req, res) => {
+  // const payload = {
+  //     _id:'635ghgvh552jhgfd',
+  //     username: 'peter',
+  //     roles: ['user']
+  // };
+  // const token = jwt.sign(payload, jwtSecret, {expiresIn: '4h'}); //next time expirins 4h
+  // res.cookie('jwt', token);
+  res.render("loginPage", {
+    title: "Login Page",
+  });
 });
 
-authController.post('/login', async (req, res) =>{
-    const result = await login(req.body.username, req.body.password);
-    const token = req.signJwt(result);
-    res.cookie('jwt', token, {maxAge: 14500000});
-    res.redirect('/');
-})
+authController.post("/login", async (req, res) => {
+    try {
+  const result = await login(req.body.username, req.body.password);
+  attachToken(req, res, result);
+  res.redirect("/");
+} catch (err) {
+    res.render("loginPage", {
+      title: "Login Page",
+      error: err.message.split("\n"),
+    });
+  }
+});
 
-authController.get('/register', (req, res) => {
-    res.render('registerPage', {
-        title:'Register Page'
-    })
-})
+authController.get("/register", (req, res) => {
+  res.render("registerPage", {
+    title: "Register Page",
+  });
+});
 
+authController.post("/register", async (req, res) => {
+    console.log(req.body)
+ try {
+   if (req.body.username.trim() == "" || req.body.password.trim() == "") {
+     throw new Error("All fields are required!");
+   }
+   if (req.body.password.trim() != req.body.repass.trim()) {
+     throw new Error("Password don't match!");
+   }
+    console.log(req.body.password.trim())
 
+    const result = await register(req.body.username.trim(), req.body.password.trim());
+    attachToken(req, res, result);
+    res.redirect("/");
+  } catch (err) {
+    res.render("registerPage", {
+      title: "Register",
+      error: err.message.split("\n"),
+    });
+  }
+});
 
-
+function attachToken(req, res, data) {
+  const token = req.signJwt(data); 
+  res.cookie('jwt', token, { maxAge: 14500000 });
+}
 
 module.exports = authController;
--- services/authService.js
+++ services/authService.js
@@ -1,17 +1,59 @@
-async function login(username, password) {
-    return new Promise((res, rej) => {
-        if(username.toLowerCase() == 'peter'&& password == '123'){
-            res({
-                _id: '29vhjvhjv1515',
-                username: 'Peter',
-                roles: ['user']
-            });
-        } else {
-            rej(new Error('Incorrect username or password'));
-        }
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+async function register(username, password){
+    //check if username is taken
+    const existing = await User.findOne({username: { $regex: new RegExp(username) , $options: 'i'}}); // options with 'i' mean keyIntensive
+    if (existing) {
+        throw new Error('Username is taken')
+
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 - How time is shake normal beetwen 9-10
+
+    // create and save  user
+    const user = await User.create({
+        username,
+        hashedPassword
     });
+    // return user data
+    return {
+        username,
+        roles: user.roles
+    }
+}
+
+async function login(username, password) {
+    const user = await User.findOne({username: { $regex: new RegExp(username) , $options: 'i'}});
+    if(!user) {
+        throw new Error('Incorrect username or password');
+    }
+
+    const match = await bcrypt.compare(password, user.hashedPassword);
+    if(!match) {
+        throw new Error('Incorrect username or password');
+    }
+
+    return {
+        username: user.username,
+        rolea: user.roles
+    };
+
+
+    //return new Promise((res, rej) => {
+    //    if(username.toLowerCase() == 'peter'&& password == '123'){
+    //        res({
+    //            _id: '29vhjvhjv1515',
+    //            username: 'Peter',
+    //            roles: ['user']
+    //        });
+    //    } else {
+    //        rej(new Error('Incorrect username or password'));
+    //    }
+    //});
 }
 
 module.exports = {
-    login
+    login,
+    register
 };
